5b5ca993600c3f4381742e4ed4b4e70cdc16ab2d
bygeorge <receipts@georgeroberts.coffee>
2022-11-07 16:29:55 -0800

    Not sure how to start this, but others have suggested starting with a Sudoku solver. Makes sense. Setting up game board

--- Sudoku/src/main/java/org/springframework/model/GameBoard.java
+++ Sudoku/src/main/java/org/springframework/model/GameBoard.java
@@ -7,4 +7,21 @@ public class GameBoard {
     BoardValues = new int[81];
   }
 
+  public int GetGameBoardValue(int x, int y){
+    int BoardCoord = (y * 9) + x;
+    return BoardValues[BoardCoord];
+  }
+
+  public int[] GetAllGameBoardValues(){
+    return BoardValues;
+  }
+
+  private boolean SetGameBoardValue(int x, int y){
+    return true;
+  }
+
+  private boolean InitiateGameBoard(){
+    return true;
+  }
+
 }
